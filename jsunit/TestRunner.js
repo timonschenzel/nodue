40e59029a37c91bb9bc1411aecd46efd507a5432
Timon Schenzel <timonschenzel@worldservants.nl>
2017-05-22 22:01:31 +0200

    JSUnit log execution time

--- jsunit/TestRunner.js
+++ jsunit/TestRunner.js
@@ -58,9 +58,13 @@ module.exports = class TestRunner
 
 	async test()
 	{
+		console.time('Time');
+
 		for (let location in this.locations) {
 			await this.runTestsInLocation(location);
 		}
+
+		console.timeEnd('Time');
 	}
 
 	async runTestsInClass(testClass, path, location)
