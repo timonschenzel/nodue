3c6e0e19ac3863abd8068e8d2b8527cdfefd26bc
Timon Schenzel <timon_schenzel@msn.com>
2017-06-02 13:23:31 +0200

    Test code style service

--- Nodue/src/App/Helpers/environment.js
+++ Nodue/src/App/Helpers/environment.js
@@ -1,12 +1,10 @@
 module.exports = {
-	env(key, override = false)
-	{
-		let value = process.env[key.toUpperCase()];
-
-		if (! value) {
-			return override;
+	env(key,override=false){
+		let value=process.env[key.toUpperCase()];
+		if(!value)
+		{
+		return override;
 		}
-
 		return value;
 	}
 }
